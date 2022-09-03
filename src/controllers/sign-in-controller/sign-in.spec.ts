1c874b0b8b98f06505ca0d61e288bf55b433a31e
Magno <magno.amaral@hvex.com.br>
2022-09-03 13:40:39 -0300

    test: ensure SignInController returns 400 if no password is provided

--- src/controllers/sign-in-controller/sign-in.spec.ts
+++ src/controllers/sign-in-controller/sign-in.spec.ts
@@ -16,4 +16,20 @@ describe('SignInController', () => {
 			message: 'Email is required'
 		});
 	});
+
+	it('should return 400 if no password is provided', async () => {
+		const sut = new SignInController();
+		const httpRequest = {
+			body: {
+				email: 'fake-mail@mail.com'
+			}
+		};
+
+		const response = await sut.handle(httpRequest);
+
+		expect(response.statusCode).toBe(400);
+		expect(response.body).toEqual({
+			message: 'Password is required'
+		});
+	});
 });
